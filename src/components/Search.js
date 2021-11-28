eae112edd751fa6414afe2a34edadfbf8eb644b5
RashBM <57621825+RashBM@users.noreply.github.com>
2021-11-28 15:35:11 +0000

    search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,62 @@
+import { useState } from "react";
+import { StyledSearch, Top } from "../styles/Search.styled";
+import Movie from "./Movie";
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3/";
+const SEARCH = `${BASE_URL}search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`;
+
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  function handleOnChange(e) {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    setSearchText(search);
+
+    const fetchData = async () => {
+      const response = await axios.get(`${SEARCH}${search}`);
+      setMovies(response.data.results);
+      console.log(response);
+
+      return response;
+    };
+    fetchData();
+
+    setSearch("");
+  }
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <StyledSearch>
+      <Top>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            onChange={handleOnChange}
+            value={search}
+          />
+        </form>
+      </Top>
+      <div>
+        <h1>Results for {searchText}</h1>
+        {movies.map((movie) => (
+          <Movie
+            heading="Featured"
+            key={movie.id}
+            title={movie.title}
+            rating={movie.vote_average}
+            image={movie.poster_path}
+            overview={movie.overview}
+          />
+        ))}
+      </div>
+    </StyledSearch>
   );
 };
 
--- src/styles/Search.styled.js
+++ src/styles/Search.styled.js
@@ -1,3 +1,19 @@
 import styled from "styled-components";
 
-export const StyledSearch = styled.div``;
+export const StyledSearch = styled.div`
+  padding-top: 8rem;
+
+  input {
+    position: fixed;
+    width: 300px;
+  }
+`;
+
+export const Top = styled.div`
+  background: yellow;
+  position: fixed;
+  top: 0;
+  padding-top: 100px;
+  padding-bottom: 50px;
+  width: 300px;
+`;
